6901437dfec065bc06cde5ab3d632269924646d3
shreya chakrabarti <shreyachakrabarti19@gmail.com>
2016-05-04 00:28:53 +0000

    pushing to heroku

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings ={
   :address => 'smtp.sendgrid.net',
   :port    => '587',
   :authentication => :plain,
-  :user_name => '##############',
-  :password => '######################',
+  :user_name => 'app48783253@heroku.com',
+  :password => '6symayz93779',
   :domain => 'heroku.com',
   :enable_starttls_auto => true
   }
